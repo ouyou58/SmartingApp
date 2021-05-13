ac541ca700dcde8bec44034ffdfe1b92b0ba02fe
ouyou58 <ouyou58@gmail.com>
2021-05-13 16:19:08 +0900

    コメントを追加

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class _BottomNavigationControllerState
     extends State<BottomNavigationController> {
   int _currentIndex = 0;
   final pages = [EquipmentManage(),BusinessName()];
+  // タブバー
   final tabBarTitle = ['備品管理', '名刺管理'];
 
   @override
