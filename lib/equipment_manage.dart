352778740908429ed5fb2476ba54ddae15b62078
name <fimmup@gmail.com>
2021-05-17 10:43:39 +0900

    Kim test

--- lib/equipment_manage.dart
+++ lib/equipment_manage.dart
@@ -3,6 +3,6 @@ import 'package:flutter/cupertino.dart';
 class EquipmentManage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('備品管理546ewe'));
+    return Center(child: Text('備品管理test'));
   }
 }
