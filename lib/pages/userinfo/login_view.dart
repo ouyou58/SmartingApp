109c6cc90eb33f5a8bbfb84c4983b463991475b0
smarting-js <fimmup@gmail.com>
2021-05-24 16:46:14 +0900

    2021/05/20 ログイン画面付け

--- lib/pages/userinfo/login_view.dart
+++ lib/pages/userinfo/login_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:smarting/Smarting_AppMenu.dart';
@@ -17,52 +18,121 @@ class LoginView extends StatefulWidget {
 class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.amber,
-        title: Text(
-          widget.title,
-          style: TextStyle(
-            color: Colors.white, fontSize: 18, fontWeight: FontWeight.w600
-          ),
+      resizeToAvoidBottomInset : false,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/bglogin.png'),
+            fit: BoxFit.fill,
+            colorFilter: ColorFilter.mode(Colors.orange, BlendMode.color)
+          )
         ),
-        actions: [
-          IconButton(icon: Icon(Icons.settings), onPressed: (){})
-        ],
-      ),
-      body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Container(
-              margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'ID',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              color: Colors.white,
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    color: Colors.orange.shade300,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          color : Color.fromRGBO(255, 153, 0, 10),
+                          blurRadius: 20.0,
+                          offset: Offset(0, 10)
+                      )
+                    ]
+                )
             ),
-            Container(
-              margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'PW',
-                  border: OutlineInputBorder(),
-                ),
+            SizedBox(height: 50),
+            Padding(
+              padding: EdgeInsets.all(45),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.all(2),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: Colors.orange.shade100,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                              color : Color.fromRGBO(255, 153, 0, 10),
+                              blurRadius: 20.0,
+                              offset: Offset(0, 10)
+                          )
+                        ]
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                            padding: EdgeInsets.all(2),
+                            child: Center(child:Image.asset('assets/images/smt_logo2.png')),
+                            decoration: BoxDecoration(
+                                color: Colors.orange.shade200,
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: [
+                                  BoxShadow(
+                                      color : Color.fromRGBO(255, 153, 0, 10),
+                                      blurRadius: 20.0,
+                                      offset: Offset(0, 10)
+                                  )
+                                ]
+                            )
+                        ),
+                        SizedBox(height: 10),
+                        Container(
+                          padding: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                              color: Colors.orange.shade50
+                          ),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "メールを入力してください。",
+                              hintStyle: TextStyle(color: Colors.grey[500]),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Container(
+                          padding: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                              color: Colors.orange.shade50
+                          ),
+                          child: TextField(
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "パスワードを入力してください。",
+                                hintStyle: TextStyle(color: Colors.grey[500])
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Container(
+                            // ignore: deprecated_member_use
+                            child: RaisedButton(
+                                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 120),
+                                color: Colors.orange,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(20))
+                                ),
+                                child: Text('ログイン', style: TextStyle(color: Colors.white),),
+                                onPressed: () {
+                                  Provider.of<PageNotifier>(context, listen: false).goToOtherPage(SmartingAppMenu.pageName);
+                                }
+                            ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              color: Colors.white,
             ),
-            RaisedButton(
-              child: Text('ログイン'),
-              onPressed: () {
-                Provider.of<PageNotifier>(context, listen: false).goToOtherPage(SmartingAppMenu.pageName);
-              }
-            )
-          ],
-        ),
+          ]
+        )
       ),
     );
   }
