8c4477702d7139afc79515532ac745edc01858de
name <fimmup@gmail.com>
2021-05-25 15:48:04 +0900

    KimJ 2021/05/25 button修正

--- lib/pages/equipment/add_new_equipment.dart
+++ lib/pages/equipment/add_new_equipment.dart
@@ -32,123 +32,125 @@ class _AddNewEquipmentState extends State<AddNewEquipment>{
                 ),
               ),
             ),
-            body : Column(
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.fromLTRB(20, 40, 20, 20),
-                  child: TextFormField(
-                    //initialValue: cate,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      labelText: 'カテゴリー',
-                      //errorText: 'Error message',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                  child: TextFormField(
-                    //initialValue: name,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      labelText: '備品名',
-                      //errorText: 'Error message',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-                InkWell(
-                  child: Container(
-                    padding: EdgeInsets.only(top: 20, bottom: 20),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black54),
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.white,
-                    ),
-                    child: Center(child: Icon(Icons.photo_camera_outlined, size: 100,),),
-                    //child: Center(child: Image.asset(pic!),),
-                    height: 200,
-                    margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                  ),
-                  onTap: () {},
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      IconButton(onPressed: () {}, icon: Icon(Icons.remove_circle)),
-                      Flexible(child:
-                      TextFormField(
-                        textAlign: TextAlign.center,
-                        inputFormatters: <TextInputFormatter>[
-                          // ignore: deprecated_member_use
-                          WhitelistingTextInputFormatter.digitsOnly
-                        ],
-                        keyboardType: TextInputType.number,
-                        initialValue: '$stock',
+            body : SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.fromLTRB(20, 30, 20, 20),
+                      child: TextFormField(
+                        //initialValue: cate,
                         decoration: InputDecoration(
                           filled: true,
                           fillColor: Colors.white,
-                          labelText: '在庫',
+                          labelText: 'カテゴリー',
+                          //errorText: 'Error message',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      child: TextFormField(
+                        //initialValue: name,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          labelText: '備品名',
+                          //errorText: 'Error message',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      child: Container(
+                        padding: EdgeInsets.only(top: 20, bottom: 20),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black54),
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white,
+                        ),
+                        child: Center(child: Icon(Icons.photo_camera_outlined, size: 100,),),
+                        //child: Center(child: Image.asset(pic!),),
+                        height: 200,
+                        margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      ),
+                      onTap: () {},
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          IconButton(onPressed: () {}, icon: Icon(Icons.remove_circle)),
+                          Flexible(child:
+                          TextFormField(
+                            textAlign: TextAlign.center,
+                            inputFormatters: <TextInputFormatter>[
+                              // ignore: deprecated_member_use
+                              WhitelistingTextInputFormatter.digitsOnly
+                            ],
+                            keyboardType: TextInputType.number,
+                            initialValue: '$stock',
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white,
+                              labelText: '在庫',
+                              //errorText: 'Error message',
+                              border: OutlineInputBorder(),
+                              counterText: '',
+                            ),
+                          )
+                          ),
+                          IconButton(onPressed: () {}, icon: Icon(Icons.add_circle))
+                        ],
+                      ),
+                      width: 180,
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      child: TextFormField(
+                        textAlignVertical: TextAlignVertical.top,
+                        maxLines: 5,
+                        //initialValue: biko,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          labelText: '備考',
                           //errorText: 'Error message',
                           border: OutlineInputBorder(),
                           counterText: '',
                         ),
-                      )
+                        maxLength: 135,
                       ),
-                      IconButton(onPressed: () {}, icon: Icon(Icons.add_circle))
-                    ],
-                  ),
-                  width: 180,
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                  child: TextFormField(
-                    textAlignVertical: TextAlignVertical.top,
-                    maxLines: 5,
-                    //initialValue: biko,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      labelText: '備考',
-                      //errorText: 'Error message',
-                      border: OutlineInputBorder(),
-                      counterText: '',
                     ),
-                    maxLength: 135,
-                  ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          CupertinoButton(
+                            child: Text('初期化'),
+                            color: Colors.black26,
+                            onPressed: () {
+                            },
+                          ),
+                          CupertinoButton(
+                            child: Text('登録'),
+                            color: Colors.red[300],
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
                 ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      CupertinoButton(
-                        child: Text('button'),
-                        color: Colors.black26,
-                        onPressed: () {
-                        },
-                      ),
-                      CupertinoButton(
-                        child: Text('button'),
-                        color: Colors.red[300],
-                        onPressed: () {},
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            )
+            ),
         )
     );
   }
--- lib/pages/equipment/equipment_info.dart
+++ lib/pages/equipment/equipment_info.dart
@@ -36,122 +36,124 @@ class _EquipmentInfoState extends State<EquipmentInfo> {
               color: Colors.white, fontSize: 18, fontWeight: FontWeight.w600),
         ),
       ),
-      body: Column(
-        //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.fromLTRB(20, 40, 20, 20),
-            child: TextFormField(
-              initialValue: _cate,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                labelText: 'カテゴリー',
-                //errorText: 'Error message',
-                border: OutlineInputBorder(),
+      body: SingleChildScrollView(
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.fromLTRB(20, 30, 20, 20),
+              child: TextFormField(
+                initialValue: _cate,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  labelText: 'カテゴリー',
+                  //errorText: 'Error message',
+                  border: OutlineInputBorder(),
+                ),
               ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-            child: TextFormField(
-              initialValue: _name,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                labelText: '備品名',
-                //errorText: 'Error message',
-                border: OutlineInputBorder(),
-              ),
-            ),
-          ),
-          InkWell(
-            child: Container(
-              padding: EdgeInsets.only(top: 20, bottom: 20),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.black54),
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white,
-              ),
-              child: Center(child: Image.asset(_pic),),
-              height: 200,
+            Container(
               margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-            ),
-            onTap: () {},
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                IconButton(onPressed: () {}, icon: Icon(Icons.remove_circle)),
-                Flexible(child:
-                TextFormField(
-                  textAlign: TextAlign.center,
-                  inputFormatters: <TextInputFormatter>[
-                    // ignore: deprecated_member_use
-                    WhitelistingTextInputFormatter.digitsOnly
-                  ],
-                  keyboardType: TextInputType.number,
-                  initialValue: '$_stock',
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    labelText: '在庫',
-                    //errorText: 'Error message',
-                    border: OutlineInputBorder(),
-                    counterText: '',
-                  ),
-                )
+              child: TextFormField(
+                initialValue: _name,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  labelText: '備品名',
+                  //errorText: 'Error message',
+                  border: OutlineInputBorder(),
                 ),
-                IconButton(onPressed: () {}, icon: Icon(Icons.add_circle))
-              ],
-            ),
-            width: 180,
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-            child: TextFormField(
-              maxLines: 5,
-              initialValue: _biko,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                labelText: '備考',
-                //errorText: 'Error message',
-                border: OutlineInputBorder(),
-                counterText: '',
-                alignLabelWithHint: true,
               ),
-              maxLength: 135,
             ),
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                CupertinoButton(
-                  child: Text('button'),
-                  color: Colors.black26,
-                  onPressed: () {
-                  },
+            InkWell(
+              child: Container(
+                padding: EdgeInsets.only(top: 20, bottom: 20),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black54),
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
                 ),
-                CupertinoButton(
-                  child: Text('button'),
-                  color: Colors.red[300],
-                  onPressed: () {},
-                ),
-              ],
+                child: Center(child: Image.asset(_pic),),
+                height: 200,
+                margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+              ),
+              onTap: () {},
             ),
-          )
-        ],
+            Container(
+              margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  IconButton(onPressed: () {}, icon: Icon(Icons.remove_circle)),
+                  Flexible(child:
+                  TextFormField(
+                    textAlign: TextAlign.center,
+                    inputFormatters: <TextInputFormatter>[
+                      // ignore: deprecated_member_use
+                      WhitelistingTextInputFormatter.digitsOnly
+                    ],
+                    keyboardType: TextInputType.number,
+                    initialValue: '$_stock',
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: '在庫',
+                      //errorText: 'Error message',
+                      border: OutlineInputBorder(),
+                      counterText: '',
+                    ),
+                  )
+                  ),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.add_circle))
+                ],
+              ),
+              width: 180,
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+              child: TextFormField(
+                maxLines: 5,
+                initialValue: _biko,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  labelText: '備考',
+                  //errorText: 'Error message',
+                  border: OutlineInputBorder(),
+                  counterText: '',
+                  alignLabelWithHint: true,
+                ),
+                maxLength: 135,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  CupertinoButton(
+                    child: Text('初期化'),
+                    color: Colors.black26,
+                    onPressed: () {
+                    },
+                  ),
+                  CupertinoButton(
+                    child: Text('登録'),
+                    color: Colors.red[300],
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
